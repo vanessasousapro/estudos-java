70f82a02a880dafd12f0fc7adbe7a5d79b264e3d
vanessasousapro <vanessasousa.pro@gmail.com>
2025-07-23 18:15:51 -0300

    first commit

---
+++ src/main/java/com/example/estudos_java/EstudosJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.estudos_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/estudos_java/EstudosJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estudos_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
